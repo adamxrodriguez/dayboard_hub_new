c81aefe876ba14e7812565deaa4e99766dc3e5aa
Adam <adamxrodriguez@gmail.com>
2024-09-12 19:34:45 -0400

    intial deploy

--- frontend/src/index.js
+++
@@ -1,8 +0,0 @@
-import React from 'react';
-import { createRoot } from 'react-dom/client';
-import App from './App';
-import './global.css';
-
-const container = document.getElementById('root');
-const root = createRoot(container); // createRoot instead of ReactDOM.render
-root.render(<App />);
